5e54280449a4c51716c08f409c4d6bf6ddd71acc
JackRyan1989 <jack.jackryan@gmail.com>
2019-10-21 15:47:15 -0400

    NavBar fanciness added.

--- resources/javascript/app.js
+++ resources/javascript/app.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
-
+    $(window).scroll(function(e) {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 10) {
+            $('#navBar').addClass("navbar-hide");
+            $("#navBar").removeClass('navbar-expand-lg');
+                anime({
+                    targets: "#brandLogo",
+                    scale: 0.5,
+                    easing: 'linear',
+                    translateX: -75,
+                    duration: 250
+                });
+        } else {
+            $("#navBar").addClass('navbar-expand-lg');
+            $('#navBar').removeClass("navbar-hide");
+            anime({
+                targets: "#brandLogo",
+                scale: 1,
+                easing: 'easeInOutQuad',
+                duration: 250
+            }) ;
+        }
+    
+    });
 
 
 
